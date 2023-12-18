d76fb935fb10315e60db2776480a93d46699ae2e
Patrick Clifford <141568205+paddy-fullstack-0417@users.noreply.github.com>
2023-12-17 20:44:21 -0500

    Modify vercel.json

--- flexion-ui/src/config/key.js
+++ flexion-ui/src/config/key.js
@@ -1,2 +1,2 @@
 // export const SERVER_ADDRESS = 'http://localhost:5000';
-export const SERVER_ADDRESS = 'https://flexion-pkuq7urwh-patts-projects.vercel.app';
+export const SERVER_ADDRESS = 'https://flexion-patts-projects.vercel.app';
