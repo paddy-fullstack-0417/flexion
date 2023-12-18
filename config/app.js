c9a955802135a7308b4cbeb3ec1d054dd3d1ec12
Patrick Clifford <141568205+paddy-fullstack-0417@users.noreply.github.com>
2023-12-18 00:53:08 -0500

    omit font-end

--- config/app.js
+++ config/app.js
@@ -14,9 +14,9 @@ app.use(cors());
 
 app.use(function(req, res, next) {
     // Set CORS headers
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
 })
 
 
--- server.js
+++ server.js
@@ -1,21 +1,29 @@
-const express = require('express');
-const path = require('path');
-const app = require('./config/app');
-const connectDB = require('./config/database');
-const { serverPort } = require('./config/key');
+import express from 'express';
+import bodyParser from 'body-parser';
+import passport from 'passport';
+import cors from 'cors';
+
+import connectDB from './config/database';
+import { serverPort } from './config/key';
+
+const app = express();
+
+require('dotenv').config();
 
 // Connect to the database
 connectDB();
 
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('flexion-ui/build'));
+app.use(cors());
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'flexion-ui', 'build', 'index.html'));
-    });
-}
+// Server static assets if in production
+// if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    // app.use(static('flexion-ui/build'));
+
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'flexion-ui', 'build', 'index.html'));
+    // });
+// }
 
 const port = process.env.PORT || serverPort;
 
